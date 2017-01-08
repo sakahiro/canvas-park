997825de9e0cbae93d9a6d1b6f591bd91855e164
榊間浩人 <h.s.bbjss383@gmail.com>
2017-01-08 19:16:23 +0900

    Introduce bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require app.bundle
+//= require bootstrap-sprockets
 //= require_tree .
---
+++ test/controllers/statics_controller_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class StaticsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get top' do
+    create_list(:work, 3)
+    get statics_top_url
+    assert_response :success
+  end
+
+  test 'should get about' do
+    get statics_about_url
+    assert_response :success
+  end
+end
