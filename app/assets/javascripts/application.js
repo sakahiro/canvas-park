b4c3145ae6aca107bf4e5693f73c4929dcc944fd
榊間浩人 <h.s.bbjss383@gmail.com>
2017-01-22 16:57:30 +0900

    Introduce dropzone for posting image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require app.bundle
 //= require bootstrap-sprockets
 //= require_tree .
---
+++ frontend/src/scripts/app.js
@@ -0,0 +1,3 @@
+const $ = global.jQuery;
+
+$(require("./postWrok"));
